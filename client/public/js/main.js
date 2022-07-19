4db632172cdfb8117d8926f40c3267007b6842f8
faizadie30 <faiz.adie30@gmail.com>
2022-07-19 18:41:50 +0700

    merge

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -1,5 +1,5 @@
 $(() => {
-  const BASE_URL = 'http://localhost:4001'
+  const BASE_URL = 'http://localhost:4000'
   const socket = io(BASE_URL, {
     withCredentials: true,
     autoConnect: true,
@@ -19,8 +19,7 @@ $(() => {
   $('#btn-login').click(function () {
     let dataAuth = {
       agent_id: '49',
-      avatar:
-        'http://localhost:8000/storage/assets/images/uploads/gravatar/agent-qwords-49.png',
+      avatar: 'http://localhost:8000/storage/assets/images/uploads/gravatar/agent-qwords-49.png',
       company_name: 'Gina Company',
       department_name: 'Developer',
       email_agent: 'agent@qwords.co.id',
@@ -33,16 +32,14 @@ $(() => {
       phone_agent: 'null',
       roles_id: '4',
       status: 'online',
-      token:
-        'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiYjRjMGRkZTBlYmRlNTczMzQ4MzU0MmNlYmQ0ZjQwOTk1YWZlOGFmZjExNTc5ZjZjZmZhODkzYTJlOGQwYjE0OThkNjBlYmM4YmU0OWRiZDciLCJpYXQiOjE2NTU5NjAyODYuOTMyMjQ2LCJuYmYiOjE2NTU5NjAyODYuOTMyMjUsImV4cCI6MTY4NzQ5NjI4Ni45MjMxOTYsInN1YiI6IjQ5Iiwic2NvcGVzIjpbXX0.QMJreS6iCrX3unIbMEw3evhyRQrJjFvfi2a-QD9ePsunDoaV0ZkKzCBizSuKAgMSbGVgoeLzsAbAcJYOx4DfTKIXP0nd-fFYH4vqDpXeCHSxX23oG9_-zMDKpvWx-AeOgm8ASV9lTG2vwIrPaAA1ygfy7OaW5-eu8PtckbDMoAyWwKupAPKXlsJzFyEO_ZU3kvj8Pss9F2MkrjZZjE8y1BmcVuhtezoiD45vZTdbbdiFet1YX3Q-JeyhXHrSaeX-MMsqKHW4B_PsEQF5UaCWbnL8IjXF2xxOtLWw3_CwdXkrZtwoev1nuNwEnYpTkCtEunIsxL6aofE32WHgQaxHk23N_Czqsd91SCK4iAa05pmWLKsYEO7m-MgAbEaIj7hiIXb_EqZTxj5ezqOoNbh-9toJUPUO0eZvNzeJTa49Fdbirr1RE6PfGnY39hvETIdgQVeBKpgC3YYC0wHiP77E2FB19_SqW5CRNTTcxq-M1TFSAFL4Jh_eEaqKryXHhdc2zewAoRT7quz29HqJUygNqzhdqxRWhOgIqqE6FLZFU758SqhMeEHgHSqyQpZE2iB_pAcBvOFcNzUEPMoobMsRVf2p6Bi_ewHLSAYjAeeBFdUUC5keXG2CBSpKEEKAxigeMR9GAlz7W7be-8J2CArlFoi08RSfQWNggPjFVHNEets',
+      token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiYjRjMGRkZTBlYmRlNTczMzQ4MzU0MmNlYmQ0ZjQwOTk1YWZlOGFmZjExNTc5ZjZjZmZhODkzYTJlOGQwYjE0OThkNjBlYmM4YmU0OWRiZDciLCJpYXQiOjE2NTU5NjAyODYuOTMyMjQ2LCJuYmYiOjE2NTU5NjAyODYuOTMyMjUsImV4cCI6MTY4NzQ5NjI4Ni45MjMxOTYsInN1YiI6IjQ5Iiwic2NvcGVzIjpbXX0.QMJreS6iCrX3unIbMEw3evhyRQrJjFvfi2a-QD9ePsunDoaV0ZkKzCBizSuKAgMSbGVgoeLzsAbAcJYOx4DfTKIXP0nd-fFYH4vqDpXeCHSxX23oG9_-zMDKpvWx-AeOgm8ASV9lTG2vwIrPaAA1ygfy7OaW5-eu8PtckbDMoAyWwKupAPKXlsJzFyEO_ZU3kvj8Pss9F2MkrjZZjE8y1BmcVuhtezoiD45vZTdbbdiFet1YX3Q-JeyhXHrSaeX-MMsqKHW4B_PsEQF5UaCWbnL8IjXF2xxOtLWw3_CwdXkrZtwoev1nuNwEnYpTkCtEunIsxL6aofE32WHgQaxHk23N_Czqsd91SCK4iAa05pmWLKsYEO7m-MgAbEaIj7hiIXb_EqZTxj5ezqOoNbh-9toJUPUO0eZvNzeJTa49Fdbirr1RE6PfGnY39hvETIdgQVeBKpgC3YYC0wHiP77E2FB19_SqW5CRNTTcxq-M1TFSAFL4Jh_eEaqKryXHhdc2zewAoRT7quz29HqJUygNqzhdqxRWhOgIqqE6FLZFU758SqhMeEHgHSqyQpZE2iB_pAcBvOFcNzUEPMoobMsRVf2p6Bi_ewHLSAYjAeeBFdUUC5keXG2CBSpKEEKAxigeMR9GAlz7W7be-8J2CArlFoi08RSfQWNggPjFVHNEets',
       type_user: 'agent',
       uuid: '5aeb48dd-5e53-11ec-93e9-e0d55e1b6010',
     };
 
     let dataAuth2 = {
       agent_id: '75',
-      avatar:
-        'http://localhost:8000/storage/assets/images/uploads/gravatar/agent-finance-1-75.png',
+      avatar: 'http://localhost:8000/storage/assets/images/uploads/gravatar/agent-finance-1-75.png',
       company_name: 'Gina Company',
       department_name: 'Finance',
       email_agent: 'agentfinance1@qwords.co.id',
@@ -55,16 +52,14 @@ $(() => {
       phone_agent: 'null',
       roles_id: '4',
       status: 'online',
-      token:
-        'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiZWYwMjVjZWViZmYwMmY2Mzc2YmMwYjg1NmUwMDEzYWFmZDNmZTI3MGRmYWIyZGQxNTFhNTYxNTRjNjcwYmVhYTlkNzhjNGZhMzkzOTI2YzYiLCJpYXQiOjE2NTU5NzQ5MzcuMTYwMjczLCJuYmYiOjE2NTU5NzQ5MzcuMTYwMjgzLCJleHAiOjE2ODc1MTA5MzcuMTQ0OTgxLCJzdWIiOiI3NSIsInNjb3BlcyI6W119.WYaFfA1Kh-3vYN8Ley4Fpcw2W3XgbcUWDAqajrs8Qi6lMRWiTMHoYoEpIt5EMd-B18_5R-hkEzUCN_aYHL4wwx1irZY-8jLG3ltYJ6PcdCayjizK3Ttu46pURT4wQSEO7554BLbOlmOw4d8xFvyu3FTYG4W4iQgr38KhINYTiQfl8K95k5mVOZejgrxNtz8eeeEHLBq17DNroK1Z0miDZe4y5BOlDqWIDoRbsEp3ISL9TcHUwYhZdHlzp98zg8RNPK2RvgdYOtq8rNvCvTCZ1Jo_JNURiRPUoQfby6V1Qg5KvmHE3OyeLFLfH0_aI526mcGIAgLbUYnxhY9gcEu04yOoAEyrqXqqNwEiv5FtzxSR5fuNnj2EUHbB_bRkQ7Eu704DWstzuNxdccS55gKpVFF5x1tw2FSes1fnBg08w1lmAdP7mUuo0Mt7MNvf5dpVjz7IzIxQCxxZ-rI5boMA_hYtfFZmpuBop2t2AmBjlGlwaJ2XZnrBpP8DWbstgjvlLJXmKOdxax6rkZUqJSszm4W5biewCHwwuVDLE3z4kB87szCRBk_3f7ip4pVctPQ0rDny97NaOUbpE5clSi68rPqgrD9PVAU28Ho7jsLEKmUG3sEfS9cT5ojvWmiiz2ajxC6GK7AMGuyXMouEDt1SNv-1e2W3Ve5hLyNTZEAYMyE',
+      token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiZWYwMjVjZWViZmYwMmY2Mzc2YmMwYjg1NmUwMDEzYWFmZDNmZTI3MGRmYWIyZGQxNTFhNTYxNTRjNjcwYmVhYTlkNzhjNGZhMzkzOTI2YzYiLCJpYXQiOjE2NTU5NzQ5MzcuMTYwMjczLCJuYmYiOjE2NTU5NzQ5MzcuMTYwMjgzLCJleHAiOjE2ODc1MTA5MzcuMTQ0OTgxLCJzdWIiOiI3NSIsInNjb3BlcyI6W119.WYaFfA1Kh-3vYN8Ley4Fpcw2W3XgbcUWDAqajrs8Qi6lMRWiTMHoYoEpIt5EMd-B18_5R-hkEzUCN_aYHL4wwx1irZY-8jLG3ltYJ6PcdCayjizK3Ttu46pURT4wQSEO7554BLbOlmOw4d8xFvyu3FTYG4W4iQgr38KhINYTiQfl8K95k5mVOZejgrxNtz8eeeEHLBq17DNroK1Z0miDZe4y5BOlDqWIDoRbsEp3ISL9TcHUwYhZdHlzp98zg8RNPK2RvgdYOtq8rNvCvTCZ1Jo_JNURiRPUoQfby6V1Qg5KvmHE3OyeLFLfH0_aI526mcGIAgLbUYnxhY9gcEu04yOoAEyrqXqqNwEiv5FtzxSR5fuNnj2EUHbB_bRkQ7Eu704DWstzuNxdccS55gKpVFF5x1tw2FSes1fnBg08w1lmAdP7mUuo0Mt7MNvf5dpVjz7IzIxQCxxZ-rI5boMA_hYtfFZmpuBop2t2AmBjlGlwaJ2XZnrBpP8DWbstgjvlLJXmKOdxax6rkZUqJSszm4W5biewCHwwuVDLE3z4kB87szCRBk_3f7ip4pVctPQ0rDny97NaOUbpE5clSi68rPqgrD9PVAU28Ho7jsLEKmUG3sEfS9cT5ojvWmiiz2ajxC6GK7AMGuyXMouEDt1SNv-1e2W3Ve5hLyNTZEAYMyE',
       type_user: 'agent',
       uuid: '3353f742-6eab-11ec-a9f4-e0d55e1b6010',
     };
 
     let dataAuth3 = {
       agent_id: '76',
-      avatar:
-        'http://localhost:8000/storage/assets/images/uploads/gravatar/agent-finance-2-76.png',
+      avatar: 'http://localhost:8000/storage/assets/images/uploads/gravatar/agent-finance-2-76.png',
       company_name: 'Gina Company',
       department_name: 'Finance',
       email_agent: 'agentfinance2@qwords.co.id',
@@ -77,34 +72,35 @@ $(() => {
       phone_agent: 'null',
       roles_id: '4',
       status: 'online',
-      token:
-        'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiNTZkNmY5OGVlMWY2ZDVmMGIxNzE5NjNjNDJjMDZiYThiNDgyYzJjM2I2NmU3YWQ4Y2YzMTQyNDQ0NTcyNDk4MjI0MGYxMzAyNmNjNGU0NjAiLCJpYXQiOjE2NTc2MDkyODAuNzU1MzE2LCJuYmYiOjE2NTc2MDkyODAuNzU1MzMzLCJleHAiOjE2ODkxNDUyODAuNzM1OTY5LCJzdWIiOiI3NiIsInNjb3BlcyI6W119.I6ZXpdfI-4wMNc0I_TaQTVeE5d0GOjkSnHnU4uPfP7_LmdwOGdyYxQ-xeAL5vncpf8PtcYeII6uZEAKX80AkZUmDZkeeoWKiNIZWCf1I9XPOBNb5FbJ6M9qqkc_XIz4wSYcedivEH9zglHaUV9RJZwwiGf46YO4F6YaGxwh2jZlyQHsj_PD5bmmh0tcaPyGOkiNEDNJ2hwj-dyh-U2y6Z5JTn8XQsjpeUVhiA0CGgmd5jUpg0To-oWyUSY9nU-FSQ0AMhVaie3Wyci5eqCkVMaOh3XFtig0gTcDI_4zRMIy8r7q_H940Ih-ndcjXQAFot8-0Aq5pyM1vYi_GfpV7T9Rb7WwW2YPLzDQqOOr9pL66nePq210Rzcs7-DKZpyQ5RlO02S1jjuNsN8lDVBj5rOjKtrPxic20Mh4HnvC0D_yc46N6k8E86VnsPdSxAMUljRyOYUGy9-ANPIh92hT4XLfeZEoQEYlrGrupa_SW79RY49n9IHXQeMHv2-5pOtFNWyKY_tPHXZbPYhqhntH38o6xFlffxjcl1gXzkLPyjig2v2trJqp9CC9niYXuDpeKBKaukWPUxsLRwFkEkpVzsLmMyqeK7EMt4D2CEnxeux2pk1PqZ9ljUhya2lDZNGEb0Eum4YGKhLAK4yUcEbNJZpl74cgxSFK9uJp5mrfYn6s',
+      token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiNTZkNmY5OGVlMWY2ZDVmMGIxNzE5NjNjNDJjMDZiYThiNDgyYzJjM2I2NmU3YWQ4Y2YzMTQyNDQ0NTcyNDk4MjI0MGYxMzAyNmNjNGU0NjAiLCJpYXQiOjE2NTc2MDkyODAuNzU1MzE2LCJuYmYiOjE2NTc2MDkyODAuNzU1MzMzLCJleHAiOjE2ODkxNDUyODAuNzM1OTY5LCJzdWIiOiI3NiIsInNjb3BlcyI6W119.I6ZXpdfI-4wMNc0I_TaQTVeE5d0GOjkSnHnU4uPfP7_LmdwOGdyYxQ-xeAL5vncpf8PtcYeII6uZEAKX80AkZUmDZkeeoWKiNIZWCf1I9XPOBNb5FbJ6M9qqkc_XIz4wSYcedivEH9zglHaUV9RJZwwiGf46YO4F6YaGxwh2jZlyQHsj_PD5bmmh0tcaPyGOkiNEDNJ2hwj-dyh-U2y6Z5JTn8XQsjpeUVhiA0CGgmd5jUpg0To-oWyUSY9nU-FSQ0AMhVaie3Wyci5eqCkVMaOh3XFtig0gTcDI_4zRMIy8r7q_H940Ih-ndcjXQAFot8-0Aq5pyM1vYi_GfpV7T9Rb7WwW2YPLzDQqOOr9pL66nePq210Rzcs7-DKZpyQ5RlO02S1jjuNsN8lDVBj5rOjKtrPxic20Mh4HnvC0D_yc46N6k8E86VnsPdSxAMUljRyOYUGy9-ANPIh92hT4XLfeZEoQEYlrGrupa_SW79RY49n9IHXQeMHv2-5pOtFNWyKY_tPHXZbPYhqhntH38o6xFlffxjcl1gXzkLPyjig2v2trJqp9CC9niYXuDpeKBKaukWPUxsLRwFkEkpVzsLmMyqeK7EMt4D2CEnxeux2pk1PqZ9ljUhya2lDZNGEb0Eum4YGKhLAK4yUcEbNJZpl74cgxSFK9uJp5mrfYn6s',
       type_user: 'agent',
       uuid: '3b1d27cd-6eab-11ec-a9f4-e0d55e1b6010',
     };
 
     let dataAuthCompany = {
-        agent_id: 10,
-        avatar: 'http://localhost:8000/storage/assets/images/uploads/gravatar/gina-dwitasari-10.png',
-        company_name: 'Gina Company',
-        department_name: null,
-        email_agent: 'gina@qwords.co.id',
-        id_company: null,
-        id_department: null,
-        name_agent: 'Gina Dwitasari',
-        permission_name: 'Company',
-        phone_agent: null,
-        roles_id: 2,
-        status: 'online',
-        token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiOTU4MGRmNDNlNDExZjIzZmFjZTJkZTFiNDEwMjI2MmQ5ZDE1YjFkNTUxMWM1NWI5ZjhlOTIwNTY0OTM4NmQyOGNkZTAxYzZmMjMxYmRmNTgiLCJpYXQiOjE2NTgxMTgyNDUuNTMyMzExLCJuYmYiOjE2NTgxMTgyNDUuNTMyMzE0LCJleHAiOjE2ODk2NTQyNDUuNTIwNywic3ViIjoiMTAiLCJzY29wZXMiOltdfQ.nnXjB-F-2Qt64nyoffH7vvbA4hs9Up9VgMrIrIkfXOn7EOasH3ah7NAlqpa3RPy3KgsSicohhYLbn-oRw3v5Fyt8kxF5NOINFASplzIR32_dEutGZl_gs3n6YPUS0ko-FN_k1n9cQGYkr6KKN5GJzGcTgdOhiq_zP5T5Rt6d_DNTkCDtkk-G6gjRje6gtfBDJ4d6GUBA38wDqaneDbxswhEE6fy96jiL1GmNZRPN41X7gfv3VFfGY3j5QjwcjT0lAuHySdpvlVsGs4JocO1KossT1MBfnw0MTGSTPBzKgt6ZyNezy89H0Lc0gZLl0aLY0Kw4SBDcvIlKrbrcw-jvKylYj6C24Il5ZSndVKIVnXvdGiJPAQ6BuuAL7GmQ1cLBtR7zu2dX9fWz5N99jn6PtQnwV1HUEvx3kIlki_aWAX_DWyOO8DvCmTwnCpr_Hk22dako7VKumkCkgRTloNFGeiod52eyVh2JtGc2CJSl9EM9j99rMqykpn9tiPchpHhiN1pAk4quR-dTK3aWq9hpvi5f7JkFtvcTnjHTrTVY2i689MJvWF-K3rweVCsAjYlHqaEJOadsSbmN7N9kG-6zg93Te723qs_h2wIsKfLfA2prn-lM35JX_Jq_fIchlSjoOtzOt_PqvtjMa0u1QRVw2ugVCTpRTYDSdosEzCf_gHQ',
-        type_user: 'company',
-        uuid: '5aeb4748-5e53-11ec-93e9-e0d55e1b6010',
+      agent_id: 10,
+      avatar: 'http://localhost:8000/storage/assets/images/uploads/gravatar/gina-dwitasari-10.png',
+      company_name: 'Gina Company',
+      department_name: null,
+      email_agent: 'gina@qwords.co.id',
+      id_company: null,
+      id_department: null,
+      name_agent: 'Gina Dwitasari',
+      permission_name: 'Company',
+      phone_agent: null,
+      roles_id: 2,
+      status: 'online',
+      token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiOTU4MGRmNDNlNDExZjIzZmFjZTJkZTFiNDEwMjI2MmQ5ZDE1YjFkNTUxMWM1NWI5ZjhlOTIwNTY0OTM4NmQyOGNkZTAxYzZmMjMxYmRmNTgiLCJpYXQiOjE2NTgxMTgyNDUuNTMyMzExLCJuYmYiOjE2NTgxMTgyNDUuNTMyMzE0LCJleHAiOjE2ODk2NTQyNDUuNTIwNywic3ViIjoiMTAiLCJzY29wZXMiOltdfQ.nnXjB-F-2Qt64nyoffH7vvbA4hs9Up9VgMrIrIkfXOn7EOasH3ah7NAlqpa3RPy3KgsSicohhYLbn-oRw3v5Fyt8kxF5NOINFASplzIR32_dEutGZl_gs3n6YPUS0ko-FN_k1n9cQGYkr6KKN5GJzGcTgdOhiq_zP5T5Rt6d_DNTkCDtkk-G6gjRje6gtfBDJ4d6GUBA38wDqaneDbxswhEE6fy96jiL1GmNZRPN41X7gfv3VFfGY3j5QjwcjT0lAuHySdpvlVsGs4JocO1KossT1MBfnw0MTGSTPBzKgt6ZyNezy89H0Lc0gZLl0aLY0Kw4SBDcvIlKrbrcw-jvKylYj6C24Il5ZSndVKIVnXvdGiJPAQ6BuuAL7GmQ1cLBtR7zu2dX9fWz5N99jn6PtQnwV1HUEvx3kIlki_aWAX_DWyOO8DvCmTwnCpr_Hk22dako7VKumkCkgRTloNFGeiod52eyVh2JtGc2CJSl9EM9j99rMqykpn9tiPchpHhiN1pAk4quR-dTK3aWq9hpvi5f7JkFtvcTnjHTrTVY2i689MJvWF-K3rweVCsAjYlHqaEJOadsSbmN7N9kG-6zg93Te723qs_h2wIsKfLfA2prn-lM35JX_Jq_fIchlSjoOtzOt_PqvtjMa0u1QRVw2ugVCTpRTYDSdosEzCf_gHQ',
+      type_user: 'company',
+      uuid: '5aeb4748-5e53-11ec-93e9-e0d55e1b6010',
     }
 
     // With fetch
     const response = fetch(`${BASE_URL}/login`, {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      headers: {
+        'Content-Type': 'application/json'
+      },
       body: JSON.stringify(dataAuthCompadany),
       credentials: 'include',
     }).then((response) => {
@@ -167,7 +163,9 @@ $(() => {
   function showLoginInfo() {
     fetch(`${BASE_URL}/login-info`, {
       method: 'GET',
-      headers: { 'Content-Type': 'application/json' },
+      headers: {
+        'Content-Type': 'application/json'
+      },
       credentials: 'include',
     }).then(async (response) => {
       response = await response.json();
@@ -200,10 +198,12 @@ $(() => {
   // Logout
   $('#btn-logout, #btn-logout-client').click(function () {
     fetch(`${BASE_URL}/logout`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      credentials: 'include',
-    })
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        credentials: 'include',
+      })
       .then(async (response) => {
         response = await response.json();
         alert(response.message);
@@ -646,24 +646,24 @@ $(() => {
     socket.emit('departments.online');
   });
 
-    /**
-     * Integrate Whatsapp Account
-     */
-    $('.btn-integrate-whatsapp').click(() => {
-        const allData = {
-            // token: $.trim($('.integrate-whatsapp-token').val()),
-            inputPhone: $.trim($('.integrate-whatsapp-input-phone').val()),
-        };
-        socket.emit('integrate.whatsapp', allData);
-    });
+  /**
+   * Integrate Whatsapp Account
+   */
+  $('.btn-integrate-whatsapp').click(() => {
+    const allData = {
+      // token: $.trim($('.integrate-whatsapp-token').val()),
+      inputPhone: $.trim($('.integrate-whatsapp-input-phone').val()),
+    };
+    socket.emit('integrate.whatsapp', allData);
+  });
 
-    socket.on('integrate.whatsappresult', (data) => {
-        console.log('Listen to integrate.whatsappresult:', data);
-    })
+  socket.on('integrate.whatsappresult', (data) => {
+    console.log('Listen to integrate.whatsappresult:', data);
+  })
 
-    socket.on('integrate.whatsapp.qr', (data) => {
-        console.log('Listen to integrate.whatsapp.qr:', data);
-    })
+  socket.on('integrate.whatsapp.qr', (data) => {
+    console.log('Listen to integrate.whatsapp.qr:', data);
+  })
 
   /** Client Area */
   /**
